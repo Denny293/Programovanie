627d8fba72300420113ae6072f7ae752e80822a3
Denny293 <111287711+Denny293@users.noreply.github.com>
2023-06-18 22:43:15 +0300

    Delete tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
- file = fopen("profile.txt" , "r"); 
- while ( fgets (balance_string, 225, file) != NULL ) {
-    count_read++;
-    if ( count_read == 2 ) {
-        puts(balance_string);
-        printf("Balance: %s", balance_string);
-    }
-      } 
-   fclose(file);
